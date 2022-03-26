5b27f5ef5bb9ee6b74b0f05a6766edb1fa249b08
surya <codewithsurya7@gmail.com>
2022-03-26 18:09:26 +0530

    Updates

---
+++ precache-manifest.e228636d78e28388e7cedc4a4f3aad5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0035da4fb5272d22a4b8",
+    "url": "/bill/static/css/main.8c6c1503.chunk.css"
+  },
+  {
+    "revision": "0035da4fb5272d22a4b8",
+    "url": "/bill/static/js/main.0035da4f.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/bill/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "f17fffd05dfa5cd0a20e",
+    "url": "/bill/static/js/2.f17fffd0.chunk.js"
+  },
+  {
+    "revision": "3f4eb0b0d1f6e07e6e00",
+    "url": "/bill/static/js/runtime~main.3f4eb0b0.js"
+  },
+  {
+    "revision": "a083eccc6fa3b7db2a7304110a8f406b",
+    "url": "/bill/index.html"
+  }
+];
